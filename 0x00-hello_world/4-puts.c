659507a31e28b4f7591d8dba4dd044b36551e5eb
Paris301 <phathutshedzomakushu@gmail.com>
2023-08-10 16:03:56 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 /**
- * main -Entry point 
+ * main - Entry point
  *
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	puts("\"Programming 	return(0);
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}
+
